99a3699a2c52f0dd8bb093bae41e421faa615d07
Ian Hundere <ianfundere@gmail.com>
2018-11-04 14:45:56 -0500

    adds boolean variable to indicate if grid needs refreshing

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const monomeGrid = require("monome-grid");
 
 async function run() {
-  let grid = await monomeGrid();
+  let grid = await monomeGrid(); // grid identifier is passed
 
+  // initialize 2d led array
   let led = [];
-  for (let y = 0; y < 15; y++) {
+  for (let y = 0; y < 16; y++) {
     led[y] = [];
-    for (let x = 0; x < 15; x++) led[y][x] = 0;
+    for (let x = 0; x < 16; x++) led[y][x] = 0;
   }
 
+  clear = true;
+
   const refresh = function() {
+    if (clear) {
+      grid.refresh(led);
+      clear = false;
+    }
     led[2][4] = 15;
     led[2][9] = 15;
     led[5][4] = 15;
@@ -21,9 +28,14 @@ async function run() {
     grid.refresh(led);
   };
 
+  // call refresh() is called 60 times per second
   setInterval(refresh, 1000 / 60);
 
-  grid.key((x, y, s) => console.log(`key received: ${x}, ${y}, ${s}`));
+  // key handler
+  grid.key((x, y, s) => {
+    led[y][x] = s * 15;
+    clear = true;
+  });
 }
 
 run();
